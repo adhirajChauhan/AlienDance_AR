7912bc0586d62b3e09ac74d5f7c66884ea08d958
Adhiraj Chauhan <adhiraj.chauhan@artisanssoft.com>
2021-01-08 17:01:42 +0530

    first commit

---
+++ Assets/Scripts/ARPlacement.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacement : MonoBehaviour
+{
+    public GameObject arObjectToSpawn;
+    public GameObject placementIndicator;
+    private GameObject spawnedObject;
+    private Pose PlacementPose;
+    private ARRaycastManager aRRaycastmanager;
+    private bool placementPoseIsValid = false;
+
+    void Start()
+    {
+        aRRaycastmanager = FindObjectOfType<ARRaycastManager>();
+    }
+
+
+    void Update()
+    {
+        if(spawnedObject == null && placementPoseIsValid && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            ARPlaceObject();
+        }
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+
+    private void UpdatePlacementIndicator()
+    {
+        if(spawnedObject == null && placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);    
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f,0.5f));
+        var hits = new List<ARRaycastHit>();
+        aRRaycastmanager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if(placementPoseIsValid)
+        {
+            PlacementPose = hits[0].pose;
+        }
+    }
+
+    void ARPlaceObject()
+    {
+        spawnedObject = Instantiate(arObjectToSpawn, PlacementPose.position, PlacementPose.rotation);
+
+    }
+}
